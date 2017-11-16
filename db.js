9ed98549853afa208df0f5ddb2e5e4d77de0cc96
Sara Vieira <hey@iamsaravieira.com>
2017-11-16 12:06:55 +0000

    add taxify

--- db.js
+++ db.js
@@ -1,14 +1,18 @@
 const uber = require('./uber.json');
 const lyft = require('./lyft.json');
 const cabify = require('./cabify.json');
+const taxify = require('./taxify.json');
 
 module.exports = () => ({
   uber: uber.cities,
   lyft: lyft.cities,
   cabify: cabify.cities,
+  taxify: taxify.cities,
   cities: [
-    ...uber.cities.map(u => ({...u, company: 'uber'})),
-    ...lyft.cities.map(u => ({...u, company: 'lyft'})),,
-    ...cabify.cities.map(u => ({...u, company: 'cabify'})),
+    ...uber.cities.map(u => ({ ...u, company: 'uber' })),
+    ...lyft.cities.map(u => ({ ...u, company: 'lyft' })),
+    ,
+    ...cabify.cities.map(u => ({ ...u, company: 'cabify' })),
+    ...taxify.cities.map(u => ({ ...u, company: 'taxify' }))
   ]
 });
--- index.js
+++ index.js
@@ -5,40 +5,86 @@ const jsonfile = require('jsonfile');
 const file = 'uber.json';
 const fileCabify = 'cabify.json';
 const fileLyft = 'lyft.json';
+const fileTaxify = 'taxify.json';
 
 const uber = { cities: [] };
 const cabify = { cities: [] };
 const lyft = { cities: [] };
+const taxify = { cities: [] };
+
+// osmosis
+//   .get('https://www.uber.com/en-GB/cities/')
+//   .find('h3 + ul li')
+//   .set({
+//     city: 'a',
+//     link: 'a@href'
+//   })
+//   .data(city => {
+//     uber.cities.push({
+//       city: city.city,
+//       link: city.link
+//     });
+//   })
+//   .error(error => error)
+//   .done(() =>
+//     jsonfile.writeFile(file, uber, function(err) {
+//       console.error(err);
+//     })
+//   );
+
+// osmosis
+//   .get('https://cabify.com/en#cities-list')
+//   .find('div.cities ul li')
+//   .set({
+//     name: 'a',
+//     link: 'a@href'
+//   })
+//   .data(city => {
+//     cabify.cities.push({
+//       name: city.name,
+//       link: city.link
+//     });
+//   })
+//   .log(console.log)
+//   .error(console.log)
+//   .debug(console.log)
+//   .done(() =>
+//     jsonfile.writeFile(fileCabify, cabify, function(err) {
+//       console.error(err);
+//     })
+//   );
+
+// osmosis
+//   .get('https://www.lyft.com/cities')
+//   .find('.list-unstyled .m-y-s')
+//   .set({
+//     name: 'a',
+//     link: 'a@href'
+//   })
+//   .data(city => {
+//     lyft.cities.push({
+//       name: city.name,
+//       link: city.link
+//     });
+//   })
+//   .log(console.log)
+//   .error(console.log)
+//   .debug(console.log)
+//   .done(() =>
+//     jsonfile.writeFile(fileLyft, lyft, function(err) {
+//       console.error(err);
+//     })
+//   );
 
 osmosis
-  .get('https://www.uber.com/en-GB/cities/')
-  .find('h3 + ul li')
-  .set({
-    city: 'a',
-    link: 'a@href'
-  })
-  .data(city => {
-    uber.cities.push({
-      city: city.city,
-      link: city.link
-    });
-  })
-  .error(error => error)
-  .done(() =>
-    jsonfile.writeFile(file, uber, function(err) {
-      console.error(err);
-    })
-  );
-
-osmosis
-  .get('https://cabify.com/en#cities-list')
-  .find('div.cities ul li')
+  .get('https://taxify.eu/cities/')
+  .find('.list-inline li h4')
   .set({
     name: 'a',
     link: 'a@href'
   })
   .data(city => {
-    cabify.cities.push({
+    taxify.cities.push({
       name: city.name,
       link: city.link
     });
@@ -47,29 +93,7 @@ osmosis
   .error(console.log)
   .debug(console.log)
   .done(() =>
-    jsonfile.writeFile(fileCabify, cabify, function(err) {
-      console.error(err);
-    })
-  );
-
-osmosis
-  .get('https://www.lyft.com/cities')
-  .find('.list-unstyled .m-y-s')
-  .set({
-    name: 'a',
-    link: 'a@href'
-  })
-  .data(city => {
-    lyft.cities.push({
-      name: city.name,
-      link: city.link
-    });
-  })
-  .log(console.log)
-  .error(console.log)
-  .debug(console.log)
-  .done(() =>
-    jsonfile.writeFile(fileLyft, lyft, function(err) {
+    jsonfile.writeFile(fileTaxify, taxify, function(err) {
       console.error(err);
     })
   );
